ec0e9bec3b3f8da03b732b76bf3a374da9d50c89
Richard Chien <richardchienthebest@gmail.com>
2020-12-12 17:30:49 +0800

    add preemptive priority policy

--- kernel/core/lab_sched.c
+++ kernel/core/lab_sched.c
@@ -10,7 +10,29 @@
 
 uint32_t pok_lab_sched_part_prio(const uint32_t index_low, const uint32_t index_high, const uint32_t prev_thread,
                                  const uint32_t current_thread) {
-    return IDLE_THREAD;
+    uint32_t t, from;
+    uint8_t max_prio = 0;
+    uint32_t max_prio_thread = IDLE_THREAD;
+
+    if (current_thread == IDLE_THREAD) {
+        from = t = prev_thread;
+    } else {
+        from = t = current_thread;
+    }
+
+    /*
+     * Walk through all threads of the given partition,
+     * select the one with highest priority.
+     */
+    do {
+        if (pok_threads[t].state == POK_STATE_RUNNABLE && pok_threads[t].priority > max_prio) {
+            max_prio = pok_threads[t].priority;
+            max_prio_thread = t;
+        }
+        t = index_low + (t - index_low + 1) % (index_high - index_low);
+    } while (t != from);
+
+    return max_prio_thread;
 }
 
 uint32_t pok_lab_sched_part_edf(const uint32_t index_low, const uint32_t index_high, const uint32_t prev_thread,
